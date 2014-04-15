327c453764b2f79c821b80c54eb71865bb820df7
Jacob Dobkins <jsd1710@gmail.com>
2014-04-15 09:22:46 -0700

    Formatted code and added project to Eclipse

--- Chaining.java
+++ Chaining.java
@@ -1,36 +1,43 @@
 
 
-public class Chaining {
+public class Chaining 
+{
 
-	static class Node {
+	static class Node 
+	{
 		Object key;
 		Object value;
                 Node next;
-        };
+    };
 
-        static public class HashTable {
-		Node [] buckets = new Node[10];
-		int size;
-		final float threshold = 0.75;
+        static public class HashTable 
+        {
+			Node [] buckets = new Node[10];
+			int size;
+			final float threshold = 0.75;
 
-		public void print() {
-			for (int i = 0; i < buckets.length; ++i) {
-				System.out.print(String.format("%4d : ", i));
-               		        node current = buckets[i];
-				while (current) {
-					System.out.print("->");
-					System.out.print(current.key);
-					System.out.print(":");
-					System.out.print(current.value);
-					current = current.next;
-				}
-				System.out.println();
-			}		
-		}
+			public void print() 
+			{
+				for (int i = 0; i < buckets.length; ++i) 
+				{
+					System.out.print(String.format("%4d : ", i));
+       		        node current = buckets[i];
+					while (current) 
+					{
+						System.out.print("->");
+						System.out.print(current.key);
+						System.out.print(":");
+						System.out.print(current.value);
+						current = current.next;
+					}
+					System.out.println();
+				}		
+			}
 
-		public Object insert(Object key, Object value) {
+		public Object insert(Object key, Object value) 
+		{
 			int code = key.hashCode();
-                        int index = code % this.size;
+            int index = code % this.size;
 
 			//TODO  insert the new object.
 			//TODO: if the load factor exceeds this.threshold, 
@@ -43,36 +50,43 @@ public class Chaining {
 		}
 
 			
-		private Node find(Object key) {
+		private Node find(Object key) 
+		{
 			
 			
 			//TODO: return a node that satidfies node.value.equals(key)
 			//      or return null if the key is not in the table.
 		}
 
-		public float loadFactor() {
+		public float loadFactor() 
+		{
 			return 0.0; //TODO:<-- replace that
 		}
 
-		public Object get(Object key, Object defaultValue) {
+		public Object get(Object key, Object defaultValue) 
+		{
 			//TODO: if the key is in the table, return it. Else return defaultvalue.
 		}
 
 
-		public boolean contains(Object key) {
+		public boolean contains(Object key) 
+		{
 			return find(key) != null;
 		}
 
 
-		private void resize(int newsize) {
+		private void resize(int newsize) 
+		{
 			Node[] oldbuckets = this.buckets
 			
 			this.buckets = new Node[newsize];
 			this.n = 0
 			
-			for (Node bucket: oldbuckets) {
+			for (Node bucket: oldbuckets) 
+			{
 				Node current = bucket;
-				while (current != null){
+				while (current != null)
+				{
 					insert(current.key, current.value);
 					current = current.next;
 				}
@@ -81,7 +95,8 @@ public class Chaining {
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Scanner s = new Scanner(System.in);
 		
 		HashTable table = new HashTable();
@@ -90,9 +105,11 @@ public class Chaining {
 		//   line into a key and value and insert into the table
 		//-->Whenever there is NOT an = sign, we lookup the entire 
 		//   line as a key. 
-		while (s.hasNextLine()){
+		while (s.hasNextLine())
+		{
 			line = s.nextLine();
-			if (line.contains("=")) {
+			if (line.contains("=")) 
+			{
 				String[] kv = line.split("=");
 				String key = kv[0].trim();
 				String value = kv[1];
@@ -100,12 +117,15 @@ public class Chaining {
 				
 				System.out.println("----------------");
 				System.out.print("inserted "+key + ":" + value);
-				if (last != null){
+				if (last != null)
+				{
 					System.out.print(" returned previous value of " + last);
 				}
 				System.out.prinrln("----------------");			
 				table.print();
-			} else {
+			} 
+			else 
+			{
 				String key = line.trim();
 				System.out.println("----------------");
 				System.out.print("Looking up " + key);
