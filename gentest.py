8d6fbb22e87e8df97b717f5e750fe7ca1031132c
John C. Femiani <john.femiani@asu.edu>
2014-04-17 09:54:42 -0600

    Added a test case generator

---
+++ gentest.py
@@ -0,0 +1,46 @@
+
+import argparse
+from random import randrange, sample, choice, shuffle
+
+p = argparse.ArgumentParser()
+
+p.add_argument("--numentries",
+               default=None,
+               type=int,
+               help="The number of key value pairs to insert")
+p.add_argument("--numqueries", default=None, type=int, help="The number of queries to make")
+p.add_argument("--interleave", default=False, type=bool, help="Set this to mix inserteions with queries; otherwise all queries will follow all insertions")
+
+args = p.parse_args()
+
+print args
+numentries = args.numentries or randrange(1, 100000)
+numqueries = args.numqueries or randrange(1, 100000)
+interleave = args.interleave
+
+
+characters = [chr(x) for x in range(ord('a'), ord('z')+1)]
+characters += [chr(x) for x in range(ord('A'), ord('Z')+1)]
+characters += [chr(x) for x in range(ord('0'), ord('9')+1)]
+characters += ['_','-']
+
+keys = []
+
+lines = []
+
+for i in range(numentries):
+    key = ''.join(sample(characters, randrange(1, 32)))
+    value = ''.join(sample(characters+[' '], randrange(1, 32)))
+    keys.append(key)
+    lines.append(key + ' '*randrange(0, 4) + '=' + value)
+
+for i in range(numqueries):
+    lines.append(choice(keys))
+
+if interleave:
+    shuffle(lines)
+
+for line in lines:
+    print line
+
+
