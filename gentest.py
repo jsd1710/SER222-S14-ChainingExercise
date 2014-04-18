f041c6847605c6817c1a71511a1131f26e44534a
John C. Femiani <john.femiani@asu.edu>
2014-04-18 14:35:57 -0700

    Added testing harness

--- gentest.py
+++ gentest.py
@@ -1,3 +1,7 @@
+"""Generate test input for the Chaining.java program."""
+
+#pylint: disable=I0011,C0103
+
 
 import argparse
 from random import randrange, sample, choice, shuffle
@@ -8,21 +12,28 @@ p.add_argument("--numentries",
                default=None,
                type=int,
                help="The number of key value pairs to insert")
-p.add_argument("--numqueries", default=None, type=int, help="The number of queries to make")
-p.add_argument("--interleave", default=False, type=bool, help="Set this to mix inserteions with queries; otherwise all queries will follow all insertions")
+p.add_argument("--numqueries",
+               default=None,
+               type=int,
+               help="The number of queries to make")
+p.add_argument("--interleave",
+               default=False,
+               type=bool,
+               help="Set this to mix inserteions with queries;"
+               + "otherwise all queries will follow all "
+               + "insertions")
 
 args = p.parse_args()
 
-print args
-numentries = args.numentries or randrange(1, 100000)
-numqueries = args.numqueries or randrange(1, 100000)
+numentries = args.numentries or randrange(1, 100)
+numqueries = args.numqueries or randrange(1, 100)
 interleave = args.interleave
 
 
-characters = [chr(x) for x in range(ord('a'), ord('z')+1)]
-characters += [chr(x) for x in range(ord('A'), ord('Z')+1)]
-characters += [chr(x) for x in range(ord('0'), ord('9')+1)]
-characters += ['_','-']
+characters = [chr(x) for x in range(ord('a'), ord('z') + 1)]
+characters += [chr(x) for x in range(ord('A'), ord('Z') + 1)]
+characters += [chr(x) for x in range(ord('0'), ord('9') + 1)]
+characters += ['_', '-']
 
 keys = []
 
@@ -30,9 +41,9 @@ lines = []
 
 for i in range(numentries):
     key = ''.join(sample(characters, randrange(1, 32)))
-    value = ''.join(sample(characters+[' '], randrange(1, 32)))
+    value = ''.join(sample(characters + [' '], randrange(1, 32)))
     keys.append(key)
-    lines.append(key + ' '*randrange(0, 4) + '=' + value)
+    lines.append(key + ' ' * randrange(0, 4) + '=' + value)
 
 for i in range(numqueries):
     lines.append(choice(keys))
@@ -42,5 +53,3 @@ if interleave:
 
 for line in lines:
     print line
-
-
